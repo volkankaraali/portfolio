47144b2910aa2b0c3b2d43925a6f229e35fa9e7c
volkankaraali <volkankaraali1@gmail.com>
2023-05-29 22:09:17 +0300

    update:seo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,8 +10,15 @@ function Layout({ children }: Props) {
   return (
     <>
       <Head>
-        <meta name="description" content="volkan karaali" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="i am volkan karaali. i'm passionate frontend developer. i am currently using react,nextjs and react-native." />
+        <meta name="keywords" content="frontend developer, frontend, developer,react,reactjs,nextjs,html,css,javascript,typescript,js,ts" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
+        <meta name="author" content="volkan karaali" />
+
         <link rel="icon" href="/code-slash.svg" />
       </Head>
       <Header />
